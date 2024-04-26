35a68cea403d7370fb3ffb185127b958635bf9a4
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-04-26 12:53:21 +0200

    feat: add increment and reset msgs for x_factor

--- src/msg.rs
+++ src/msg.rs
@@ -4,13 +4,16 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
     pub count: i32,
+    pub x_factor: i32,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
     Increment {},
+    IncrementXFactor {},
     Reset { count: i32 },
+    ResetXFactor { x_factor: i32 },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
@@ -18,10 +21,16 @@ pub enum ExecuteMsg {
 pub enum QueryMsg {
     // GetCount returns the current count as a json-encoded number
     GetCount {},
+    GetXFactor {},
 }
 
 // We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
 pub struct CountResponse {
     pub count: i32,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
+pub struct XFactorResponse {
+    pub x_factor: i32,
 }
